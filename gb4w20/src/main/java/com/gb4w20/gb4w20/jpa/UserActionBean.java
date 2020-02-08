0233b5e6d43333834c37eac9f21a0ff0f4c81234
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-02-08 14:23:07 -0500

    Began user action bean.

---
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/jpa/UserActionBean.java
@@ -0,0 +1,45 @@
+
+package com.gb4w20.gb4w20.jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Used as a DAO for the users table. 
+ * 
+ * @author Jeffrey Boisvert
+ */
+public class UserActionBean implements Serializable{
+    
+    private final static Logger LOG = LoggerFactory.getLogger(UserActionBean.class);
+
+    @Resource
+    private UserTransaction userTransaction;
+
+    @PersistenceContext(unitName = "usersPU")
+    private EntityManager entityManager;
+    
+    public UserActionBean(){
+        //Left empty to comply with frameworks. 
+    }
+    
+    
+    
+}
