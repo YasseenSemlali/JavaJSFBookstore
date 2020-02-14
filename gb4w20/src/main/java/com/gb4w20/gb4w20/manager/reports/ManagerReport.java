eac6562206729261d581e84304bfec412344ad78
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-02-13 21:52:38 -0500

    Created ManagerReport to hold logic of generating reports.

---
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/manager/reports/ManagerReport.java
@@ -0,0 +1,11 @@
+
+package com.gb4w20.gb4w20.manager.reports;
+
+/**
+ * Class holding useful methods for manager reports. 
+ * 
+ * @author Jeffrey Boisvert
+ */
+public class ManagerReport {
+    
+}
