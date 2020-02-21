d5770f4e124b73bba66140be2dd696deef745659
jasmarbadion <jasmar.badion@gmail.com>
2020-02-21 18:09:08 -0500

    added the bundles and showing 3 other books of same author when a book has more than 1 author works now. Need to fix submit review and showing 3 other books of same genre but different author when there are more than 1 author

--- gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/BookReviewBackingBean.java
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/BookReviewBackingBean.java
@@ -6,11 +6,13 @@ package com.gb4w20.gb4w20.backingbeans;
 import com.gb4w20.gb4w20.entities.Books;
 import com.gb4w20.gb4w20.entities.Reviews;
 import com.gb4w20.gb4w20.entities.Users;
+import com.gb4w20.gb4w20.jpa.ReviewsJpaController;
 import java.io.Serializable;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * <h1>Book Review Backing Bean</h1>
@@ -28,11 +30,14 @@ public class BookReviewBackingBean implements Serializable{
     
     private final static Logger LOG = LoggerFactory.getLogger(BookReviewBackingBean.class);
     
-    private Reviews reviews;
+    //private Reviews reviews;
+    protected String review;
+    protected short rating;
     
-    //default constructor
+    @Inject
+    private ReviewsJpaController reviewsJpaController;
+    
     public BookReviewBackingBean(){}
-    
     /**
      * Creates a review created by a logged in user to a specific book
      * in the book page
@@ -41,30 +46,49 @@ public class BookReviewBackingBean implements Serializable{
      * @param user
      * @return 
      */
-    public Reviews getReview(Books book, Users user){
+    public void getReview(Books book, Users user){
         LOG.info("Review being created from the book page");
         
-        if(this.reviews == null){
+        /*if(this.reviews == null){
             this.reviews = new Reviews();  
             this.reviews.setIsbn(book);
             this.reviews.setUserId(user);
         }
-        return this.reviews; 
+        return this.reviews; */
+        Reviews rev = new Reviews();
+        rev.setReview(review);
+        rev.setRating(rating);
+        rev.setUserId(user);
+        rev.setIsbn(book);
+        
+        reviewsJpaController.create(rev);
+        
     }
-    
+    public String getReview(){
+        return this.review;
+    }
+    public void setReview(String review){
+        this.review = review;
+    }
+    public short getRating(){
+        return this.rating;
+    }
+    public void setRating(short rating){
+        this.rating = rating;
+    }
     /**
      * Getter for reviews
      * @return 
      */
-    public Reviews getReviews(){
+    /*public Reviews getReviews(){
         return this.reviews;
-    }
+    }*/
     
     /**
      * Setter for reviews
      * @param reviews 
      */
-    public void setReviews(Reviews reviews){
+    /*public void setReviews(Reviews reviews){
         this.reviews = reviews;
-    }
+    }*/
 }
