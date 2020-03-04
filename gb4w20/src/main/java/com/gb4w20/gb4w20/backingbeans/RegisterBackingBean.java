8f03482be095e4a4cd4484b571f8df76a90dfa02
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-03-03 22:27:58 -0500

    Added basic setter and getters for all values to create a user.

--- gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/RegisterBackingBean.java
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/RegisterBackingBean.java
@@ -36,8 +36,21 @@ public class RegisterBackingBean implements Serializable {
     @Inject
     UsersJpaController userJpaController;
     
+    //Lists for selections
     private List<String> availableTitles; 
+    
+    //Inputs
     private String titleInput; 
+    private String firstNameInput;
+    private String lastNameInput;
+    private String firstAddressInput;
+    private String secondAddressInput;
+    private String cityInput; 
+    private String provinceInput;
+    private String countryInput;
+    private String postalInput;
+    private String mobilePhoneInput; 
+    private String homePhoneInput;
     private String emailInput; 
     private String passwordInput;
     
@@ -114,6 +127,216 @@ public class RegisterBackingBean implements Serializable {
      */
     public void setPasswordInput(String passwordInput){
         this.passwordInput = passwordInput; 
+    }
+    
+    /**
+     * Getting for the firstNameInput and sets value to empty string if null
+     * @return firstNameInput value
+     * @author Jeffrey Boisvert
+     */
+    public String getFirstNameInput() {
+        if(this.firstNameInput == null){
+            return ""; 
+        }
+        return firstNameInput;
+    }
+    
+    /**
+     * Setter for the firstNameInput
+     * @param firstNameInput 
+     * @author Jeffrey Boisvert
+     */
+    public void setFirstNameInput(String firstNameInput) {
+        this.firstNameInput = firstNameInput;
+    }
+    
+    /**
+     * Getter for lastNameInput and returns empty string if null
+     * @return value of lastNameInput
+     * @author Jeffrey Boisvert
+     */
+    public String getLastNameInput() {
+        if(this.lastNameInput == null){
+            return ""; 
+        }
+        return lastNameInput;
+    }
+    
+    /**
+     * Setter for the lastNameInput
+     * @param lastNameInput
+     * @author Jeffrey Boisvert
+     */
+    public void setLastNameInput(String lastNameInput) {
+        this.lastNameInput = lastNameInput;
+    }
+    
+    /**
+     * Used to get the firstAddressInput value inputted
+     * @return String of firstAddressInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getFirstAddressInput() {
+        if(this.firstAddressInput == null){
+            return ""; 
+        }
+        return firstAddressInput;
+    }
+
+    /**
+     * Used to set the firstAddressInput to the value given
+     * @param firstAddressInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setFirstAddressInput(String firstAddressInput) {
+        this.firstAddressInput = firstAddressInput;
+    }
+    
+    /**
+     * Used to get the secondAddressInput value inputted
+     * @return String of secondAddressInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getSecondAddressInput() {
+        if(this.secondAddressInput == null){
+            return ""; 
+        }
+        return secondAddressInput;
+    }
+    
+    /**
+     * Used to set the secondAddressInput to the value given
+     * @param secondAddressInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setSecondAddressInput(String secondAddressInput) {
+        this.secondAddressInput = secondAddressInput;
+    }
+    
+    /**
+     * Used to get the cityInput value inputted
+     * @return String of cityInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getCityInput() {
+        if(this.cityInput == null){
+            return ""; 
+        }
+        return cityInput;
+    }
+    
+    /**
+     * Used to set the cityInput to the value given
+     * @param city given
+     * @author Jeffrey Boisvert 
+     */
+    public void setCity(String city) {
+        this.cityInput = city;
+    }
+    
+    /**
+     * Used to get the provinceInput value inputted
+     * @return String of provinceInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getProvinceInput() {
+        if(this.provinceInput == null){
+            return ""; 
+        }
+        return provinceInput;
+    }
+    
+    /**
+     * Used to set the provinceInput to the value given
+     * @param provinceInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setProvinceInput(String provinceInput) {
+        this.provinceInput = provinceInput;
+    }
+    
+    /**
+     * Used to get the countryInput value inputted
+     * @return String of countryInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getCountryInput() {
+        if(this.countryInput == null){
+            return ""; 
+        }
+        return countryInput;
+    }
+    
+    /**
+     * Used to set the countryInput to the value given
+     * @param countryInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setCountryInput(String countryInput) {
+        this.countryInput = countryInput;
+    }
+    
+    /**
+     * Used to get the getPostalInput value inputted
+     * @return String of getPostalInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getPostalInput() {
+        if(this.postalInput == null){
+            return ""; 
+        }
+        return postalInput;
+    }
+    
+    /**
+     * Used to set the postalInput to the value given
+     * @param postalInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setPostalInput(String postalInput) {
+        this.postalInput = postalInput;
+    }
+    
+    /**
+     * Used to get the mobilePhoneInput value inputted
+     * @return String of the inputted mobilePhoneInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getMobilePhoneInput() {
+        if(this.mobilePhoneInput == null){
+            return ""; 
+        }
+        return mobilePhoneInput;
+    }
+    
+    /**
+     * Used to set the mobilePhoneInput to the value given
+     * @param mobilePhoneInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setMobilePhoneInput(String mobilePhoneInput) {
+        this.mobilePhoneInput = mobilePhoneInput;
+    }
+    
+    /**
+     * Used to get the homePhoneInput value inputted
+     * @return String of the inputted homePhoneInput.
+     * @author Jeffrey Boisvert
+     */
+    public String getHomePhoneInput() {
+        if(this.homePhoneInput == null){
+            return ""; 
+        }
+        return homePhoneInput;
+    }
+    
+    /**
+     * Used to set the homePhoneInput to the value given
+     * @param homePhoneInput given
+     * @author Jeffrey Boisvert 
+     */
+    public void setHomePhoneInput(String homePhoneInput) {
+        this.homePhoneInput = homePhoneInput;
     }
     
     /**
