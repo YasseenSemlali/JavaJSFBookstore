b44c94a3dd4c5c72f83654c47babcf0cdcc58d81
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-03-07 16:59:35 -0500

    top sellers report has error message and ajax report running.

--- gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/TopSellersReportBackingBean.java
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/TopSellersReportBackingBean.java
@@ -6,7 +6,11 @@ import com.gb4w20.gb4w20.jpa.UsersJpaController;
 import com.gb4w20.gb4w20.querybeans.NameAndNumberBean;
 import java.io.Serializable;
 import java.util.List;
+import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.slf4j.Logger;
@@ -32,6 +36,19 @@ public class TopSellersReportBackingBean implements Serializable {
     
     private List<NameAndNumberBean> bookSales; 
     
+    //Bundle for i18n
+    private ResourceBundle bundle; 
+    
+    /**
+     * Mainly used to set default values.
+     * @author Jeffrey Boisvert
+     */
+    @PostConstruct
+    public void init(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        this.bundle = context.getApplication().getResourceBundle(context, "msgs");
+    }
+    
     /**
      * This will set the properties of the bean of for the book sales based
      * on the values set in startDate and endDate. 
@@ -45,7 +62,22 @@ public class TopSellersReportBackingBean implements Serializable {
      * Helper method to set the list of clients and sales in a given date range.
      */
     private void setBookSales() {
-        this.bookSales = this.booksJpaController.findTopSellers(sqlDate(this.startDate).toString(), sqlDate(this.endDate).toString());
+        
+        try{
+            
+            this.bookSales = this.booksJpaController.findTopSellers(sqlDate(this.startDate).toString(), sqlDate(this.endDate).toString());
+
+            if(this.bookSales.isEmpty()){
+                FacesMessage message = new FacesMessage(this.bundle.getString("report_no_result"));
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+        catch (Exception ex){
+            LOG.debug("Error running report ", ex);
+            FacesMessage message = new FacesMessage(this.bundle.getString("error_running_report"));
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+        
     }
     
     /**
