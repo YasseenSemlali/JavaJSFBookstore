40d540a8dba73166c737ee8b97b1502c715912ff
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-03-07 20:09:59 -0500

    Fixed zero sales report.

--- gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/ZeroSalesReportBackingBean.java
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/backingbeans/ZeroSalesReportBackingBean.java
@@ -5,7 +5,11 @@ import com.gb4w20.gb4w20.entities.Books;
 import com.gb4w20.gb4w20.jpa.BooksJpaController;
 import java.io.Serializable;
 import java.util.List;
+import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.slf4j.Logger;
@@ -31,12 +35,52 @@ public class ZeroSalesReportBackingBean implements Serializable {
     
     private List<Books> bookSales; 
     
+    //Bundle for i18n
+    private ResourceBundle bundle; 
+    
+    /**
+     * Mainly used to set default values.
+     * @author Jeffrey Boisvert
+     */
+    @PostConstruct
+    public void init(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        this.bundle = context.getApplication().getResourceBundle(context, "msgs");
+    }
+    
     /**
      * This will set the properties of the bean of for the book sales based
      * on the values set in startDate and endDate. 
      */
     public void runReport(){
-        setBookSales();
+        
+                if(this.startDate == null || this.endDate == null){
+            FacesMessage message = new FacesMessage(this.bundle.getString("missing_dates_error"));
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            return;
+        }
+        
+        if(this.startDate.after(this.endDate)){
+            FacesMessage message = new FacesMessage(this.bundle.getString("start_date_after_end_date_error"));
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            return;
+        }
+        
+        try{
+            
+            setBookSales();
+            
+            if(this.bookSales.isEmpty()){
+                FacesMessage message = new FacesMessage(this.bundle.getString("report_no_result"));
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+        catch (Exception ex){
+            LOG.debug("Error running report ", ex);
+            FacesMessage message = new FacesMessage(this.bundle.getString("error_running_report"));
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+        
     }
     
     
