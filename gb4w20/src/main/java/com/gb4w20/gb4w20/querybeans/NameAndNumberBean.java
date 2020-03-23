522c687e29daf3c1d62d7e891b09e09bd9f6f029
yasseen <yasseen2000@gmail.com>
2020-03-23 18:05:46 -0400

    did most of usertests, added a result option to parameter rule

--- gb4w20/src/main/java/com/gb4w20/gb4w20/querybeans/NameAndNumberBean.java
+++ gb4w20/src/main/java/com/gb4w20/gb4w20/querybeans/NameAndNumberBean.java
@@ -1,25 +1,26 @@
-
 package com.gb4w20.gb4w20.querybeans;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
- * This is simply used to be able to do group by name, total amount, and count for reports. 
- * 
+ * This is simply used to be able to do group by name, total amount, and count
+ * for reports.
+ *
  * @author Jeffrey Boisvert
  */
 public class NameAndNumberBean implements Serializable {
-    
+
     private String name;
-    private BigDecimal amount; 
-    
-    public NameAndNumberBean(){
+    private BigDecimal amount;
+
+    public NameAndNumberBean() {
     }
-        
-    public NameAndNumberBean(String name, BigDecimal amount){
-        this.name = name; 
-        this.amount = amount; 
+
+    public NameAndNumberBean(String name, BigDecimal amount) {
+        this.name = name;
+        this.amount = amount;
     }
 
     public BigDecimal getAmount() {
@@ -29,13 +30,41 @@ public class NameAndNumberBean implements Serializable {
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String value) {
         name = value;
     }
+
+    @Override
+    public String toString() {
+        return "{" + name + ", " + amount + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final NameAndNumberBean other = (NameAndNumberBean) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.amount, other.amount)) {
+            return false;
+        }
+        return true;
+    }
     
+    
+
 }
