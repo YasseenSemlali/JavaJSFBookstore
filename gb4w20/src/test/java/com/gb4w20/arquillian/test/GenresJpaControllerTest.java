de63568930aaafdf20c5fe2347793ff2b1c680f6
jasmarbadion <jasmar.badion@gmail.com>
2020-03-22 23:41:44 -0400

    finished tests for authors and genres controller, going to work on rss and reviews test next

--- gb4w20/src/test/java/com/gb4w20/arquillian/test/GenresJpaControllerTest.java
+++ gb4w20/src/test/java/com/gb4w20/arquillian/test/GenresJpaControllerTest.java
@@ -37,7 +37,6 @@ public class GenresJpaControllerTest {
         @Rule
         public ParameterRule rule = new ParameterRule("param",
                 //test number, isbn, genreId, authorId, maxResults, expected resultset size
-<<<<<<< HEAD
                 new Sextet<Integer, Long, Collection<Genres>, Collection<Authors>, Integer, Integer>(1, 9780000000006l, new ArrayList<Genres>(Arrays.asList(new Genres(1l))), new ArrayList<Authors>(Arrays.asList(new Authors(6l))), 3, 3),
                 new Sextet<Integer, Long, Collection<Genres>, Collection<Authors>, Integer, Integer>(2, 9780000000010l, new ArrayList<Genres>(Arrays.asList(new Genres(2l))), new ArrayList<Authors>(Arrays.asList(new Authors(8l))), 7, 2),
                 new Sextet<Integer, Long, Collection<Genres>, Collection<Authors>, Integer, Integer>(3, 9780316251303l, new ArrayList<Genres>(Arrays.asList(new Genres(1l))), new ArrayList<Authors>(Arrays.asList(new Authors(5l))), 7, 4),
