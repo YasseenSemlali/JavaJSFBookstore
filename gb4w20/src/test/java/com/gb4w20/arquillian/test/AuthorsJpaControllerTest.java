245a95edf0f9eafe4ece1ea83848dbe646d4233b
jasmarbadion <jasmar.badion@gmail.com>
2020-03-24 13:51:21 -0400

    done with rss test and also updated my ParameterRule params test for authors test and genres test so that it takes the result of the method being tested as part of the param

--- gb4w20/src/test/java/com/gb4w20/arquillian/test/AuthorsJpaControllerTest.java
+++ gb4w20/src/test/java/com/gb4w20/arquillian/test/AuthorsJpaControllerTest.java
@@ -1,5 +1,6 @@
 package com.gb4w20.arquillian.test;
 
+import com.gb4w20.arquillian.test.rules.ParameterRule;
 import com.gb4w20.gb4w20.entities.Authors;
 import com.gb4w20.gb4w20.entities.Books;
 import com.gb4w20.gb4w20.jpa.AuthorsJpaController;
@@ -48,17 +49,19 @@ public class AuthorsJpaControllerTest {
         @Inject
         private AuthorsJpaController authorsJpaController;
         
-        @Rule
-        public ParameterRule rule = new ParameterRule("param",
-                //test number, id, startDate, endDate, expected result
-                new Quintet<Integer, Long, String, String, Double>(1, 1l, "2020-01-23", "2020-03-21", 64.00),
-                new Quintet<Integer, Long, String, String, Double>(2, 2l, "2020-01-23", "2020-03-21", 34.00),
-                new Quintet<Integer, Long, String, String, Double>(3, 3l, "2020-01-23", "2020-03-21", 56.00),
-                new Quintet<Integer, Long, String, String, Double>(4, 6l, "2020-01-01", "2020-03-21", 24.00),
-                new Quintet<Integer, Long, String, String, Double>(5, 9l, "2020-01-01", "2020-03-21", 10.00)
-                );
-        
         private Quintet<Integer, Long, String, String, Double> param;
+        
+        //holds the result of the method being tested
+        private double result;
+        
+        @Rule
+        public ParameterRule<Quintet<Integer, Long, String, String, Double>> rule = new ParameterRule("param", "result",
+                () -> authorsJpaController.getAuthorsTotalSales(param.getValue1(), param.getValue2(), param.getValue3()),
+                Quintet.with(1, 1l, "2020-01-23", "2020-03-21", 64.00),
+                Quintet.with(2, 2l, "2020-01-23", "2020-03-21", 34.00),
+                Quintet.with(3, 3l, "2020-01-23", "2020-03-21", 56.00),
+                Quintet.with(4, 6l, "2020-01-01", "2020-03-21", 24.00),
+                Quintet.with(5, 9l, "2020-01-01", "2020-03-21", 10.00));
         
         /**
          * Used to test if the total sales given is equals to
@@ -68,14 +71,9 @@ public class AuthorsJpaControllerTest {
         @Test
         public void testCorrectTotalSales(){
             int testNumber = param.getValue0();
-            long authorId = param.getValue1();
-            String startDate = param.getValue2();
-            String endDate = param.getValue3();
             double expectedTotalSales = param.getValue4();
             
-            double totalSales = authorsJpaController.getAuthorsTotalSales(authorId, startDate, endDate);
-            
-            assertEquals("Test " + testNumber + " did not return the correct total sales of this author", expectedTotalSales, totalSales, 0.01f);
+            assertEquals("Test " + testNumber + " did not return the correct total sales of this author", expectedTotalSales, this.result, 0.01f);
         }
     }
     
@@ -88,34 +86,20 @@ public class AuthorsJpaControllerTest {
 
         @Inject
         private AuthorsJpaController authorsJpaController;
-        
-        @Rule
-        public ParameterRule rule = new ParameterRule("param",
-                //test number, id, startDate, endDate, expected result
-                new Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>>(1, 1l, "2020-01-23", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Good Omens: The Nice and Accurate Prophecies of Agnes Nutter, Witch", new BigDecimal("34.00"), 2l), new NameTotalAndCountBean("Harry Potter and the Deathly Hallows", new BigDecimal("30.00"), 2l)))),
-                new Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>>(2, 2l, "2020-01-23", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Good Omens: The Nice and Accurate Prophecies of Agnes Nutter, Witch", new BigDecimal("34.00"), 2l)))),
-                new Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>>(3, 3l, "2020-01-23", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Harry Potter and the Chamber of Secrets", new BigDecimal("26.00"), 2l), new NameTotalAndCountBean("Harry Potter and the Deathly Hallows", new BigDecimal("30.00"), 2l)))),
-                new Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>>(4, 6l, "2020-01-01", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Red Seas Under Red Skies", new BigDecimal("24.00"), 2l)))),
-                new Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>>(3, 9l, "2020-01-01", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("And Another Thing...", new BigDecimal("10.00"), 1l))))
-                );
-        
+               
         private Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>> param;
         
         //holds the result of the method being tested
-        private List<NameTotalAndCountBean> purchasedBooks;
+        private List<NameTotalAndCountBean> result;
         
-        /**
-         * This method will hold the result of the method being tested
-         * @author Jasmar Badion
-         */
-        @Before
-        public void initializeTheTestedMethodResult(){
-            long authorId = param.getValue1();
-            String startDate = param.getValue2();
-            String endDate = param.getValue3();
-            
-            this.purchasedBooks = authorsJpaController.getPurchasedBooksByAuthor(authorId, startDate, endDate);
-        }
+        @Rule
+        public ParameterRule<Quintet<Integer, Long, String, String, List<NameTotalAndCountBean>>> rule = new ParameterRule("param", "result",
+                () -> authorsJpaController.getPurchasedBooksByAuthor(param.getValue1(), param.getValue2(), param.getValue3()),
+                Quintet.with(1, 1l, "2020-01-23", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Good Omens: The Nice and Accurate Prophecies of Agnes Nutter, Witch", new BigDecimal("34.00"), 2l), new NameTotalAndCountBean("Harry Potter and the Deathly Hallows", new BigDecimal("30.00"), 2l)))),
+                Quintet.with(2, 2l, "2020-01-23", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Good Omens: The Nice and Accurate Prophecies of Agnes Nutter, Witch", new BigDecimal("34.00"), 2l)))),
+                Quintet.with(3, 3l, "2020-01-23", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Harry Potter and the Chamber of Secrets", new BigDecimal("26.00"), 2l), new NameTotalAndCountBean("Harry Potter and the Deathly Hallows", new BigDecimal("30.00"), 2l)))),
+                Quintet.with(4, 6l, "2020-01-01", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("Red Seas Under Red Skies", new BigDecimal("24.00"), 2l)))),
+                Quintet.with(5, 9l, "2020-01-01", "2020-03-21", new ArrayList<NameTotalAndCountBean>(Arrays.asList(new NameTotalAndCountBean("And Another Thing...", new BigDecimal("10.00"), 1l)))));
         
         /**
          * Used to test if the number of books purchased given is equals to
@@ -127,7 +111,7 @@ public class AuthorsJpaControllerTest {
             int testNumber = param.getValue0();
             List<NameTotalAndCountBean> expectedPurchasedBooks = param.getValue4();
             
-            assertEquals("Test " + testNumber + " did not return the correct number of purchased books of this author", expectedPurchasedBooks.size(), this.purchasedBooks.size());
+            assertEquals("Test " + testNumber + " did not return the correct number of purchased books of this author", expectedPurchasedBooks.size(), this.result.size());
         }
         
         /**
@@ -142,7 +126,7 @@ public class AuthorsJpaControllerTest {
             
             List<String> expectedTitles = takeTitlesFromPurchasedBooks(expectedPurchasedBooks);
            
-            assertTrue("Test " + testNumber + " does not contain expected books", expectedTitles.containsAll(takeTitlesFromPurchasedBooks(this.purchasedBooks)));
+            assertTrue("Test " + testNumber + " does not contain expected books", expectedTitles.containsAll(takeTitlesFromPurchasedBooks(this.result)));
         }
         
         /**
@@ -172,34 +156,20 @@ public class AuthorsJpaControllerTest {
         @Inject
         private AuthorsJpaController authorsJpaController;
         
-        @Rule
-        public ParameterRule rule = new ParameterRule("param",
-                //test number, isbn, authors, maxResult, expected result
-                new Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>>(1, 9780000000000l, new ArrayList<Authors>(Arrays.asList(new Authors(1l), new Authors(2l))), 3, new ArrayList<Books>(Arrays.asList(new Books(9780545010221l)))),
-                new Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>>(2, 9780439064866l, new ArrayList<Authors>(Arrays.asList(new Authors(3l), new Authors(4l))), 3, new ArrayList<Books>(Arrays.asList(new Books(9780545010221l)))),
-                new Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>>(3, 9780545010221l, new ArrayList<Authors>(Arrays.asList(new Authors(3l), new Authors(1l))), 3, new ArrayList<Books>(Arrays.asList(new Books(9780000000000l), new Books(9780439064866l)))),
-                new Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>>(4, 9780316251303l, new ArrayList<Authors>(Arrays.asList(new Authors(5l))), 3, new ArrayList<Books>()),
-                new Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>>(5, 9781401323585l, new ArrayList<Authors>(Arrays.asList(new Authors(9l))), 1, new ArrayList<Books>())
-                );
-        
         private Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>> param;
         
         //holds the result of the method being tested
-        private List<Books> otherBooks;
+        private List<Books> result;
         
-        /**
-         * This will initialize the result of the method being tested
-         * @author Jasmar Badion
-         */
-        @Before
-        public void initializeTheTestedMethodResult(){
-            long isbn = param.getValue1();
-            Collection<Authors> authors = param.getValue2();
-            int maxResult = param.getValue3();
-            
-            this.otherBooks = authorsJpaController.getOtherBooksBySameAuthor(isbn, authors, maxResult);
-        }
-        
+        @Rule
+        public ParameterRule<Quintet<Integer, Long, Collection<Authors>, Integer, List<Books>>> rule = new ParameterRule("param", "result",
+                () -> authorsJpaController.getOtherBooksBySameAuthor(param.getValue1(), param.getValue2(), param.getValue3()),
+                Quintet.with(1, 9780000000000l, new ArrayList<Authors>(Arrays.asList(new Authors(1l), new Authors(2l))), 3, new ArrayList<Books>(Arrays.asList(new Books(9780545010221l)))),
+                Quintet.with(2, 9780439064866l, new ArrayList<Authors>(Arrays.asList(new Authors(3l), new Authors(4l))), 3, new ArrayList<Books>(Arrays.asList(new Books(9780545010221l)))),
+                Quintet.with(3, 9780545010221l, new ArrayList<Authors>(Arrays.asList(new Authors(3l), new Authors(1l))), 3, new ArrayList<Books>(Arrays.asList(new Books(9780000000000l), new Books(9780439064866l)))),
+                Quintet.with(4, 9780316251303l, new ArrayList<Authors>(Arrays.asList(new Authors(5l))), 3, new ArrayList<Books>()),
+                Quintet.with(5, 9781401323585l, new ArrayList<Authors>(Arrays.asList(new Authors(9l))), 1, new ArrayList<Books>()));
+               
         /**
          * Used to test if the number of books by same author given is equals to
          * the expected number of books by same author of a chosen book
@@ -210,7 +180,7 @@ public class AuthorsJpaControllerTest {
             int testNumber = param.getValue0();
             List<Books> expectedOtherBooks = param.getValue4();
             
-            assertEquals("Test " + testNumber + " did not return the correct number of books by same authors", expectedOtherBooks.size(), this.otherBooks.size());
+            assertEquals("Test " + testNumber + " did not return the correct number of books by same authors", expectedOtherBooks.size(), this.result.size());
         }
         
         /**
@@ -223,7 +193,7 @@ public class AuthorsJpaControllerTest {
             int testNumber = param.getValue0();
             List<Books> expectedOtherBooks = param.getValue4();
             
-            assertEquals("Test " + testNumber + " did not return the correct other books by same authors", expectedOtherBooks, this.otherBooks);
+            assertEquals("Test " + testNumber + " did not return the correct other books by same authors", expectedOtherBooks, this.result);
         }
         
         /**
@@ -235,7 +205,7 @@ public class AuthorsJpaControllerTest {
             int testNumber = param.getValue0();
             long isbn = param.getValue1();
             
-            assertTrue( "Test " + testNumber + ": The same book is present in the result set", booksDoNotContainTheGivenIsbn(this.otherBooks, isbn));
+            assertTrue( "Test " + testNumber + ": The same book is present in the result set", booksDoNotContainTheGivenIsbn(this.result, isbn));
         }
         
         /**
